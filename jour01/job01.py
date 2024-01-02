557f6ab8199de53ede9f4e9e1b94946ad6992af6
jean-renoul <jean.renoul@laplateforme.io>
2024-01-02 09:57:17 +0100

    job01 et 02

---
+++ jour01/job01.py
@@ -0,0 +1,7 @@
+class operation:
+    def __init__(self, nombre1, nombre2):
+        self.nombre1 = nombre1
+        self.nombre2 = nombre2
+
+exemple = operation (10,5)
+print(exemple)
---
+++ jour01/job02.py
@@ -0,0 +1,8 @@
+class operation:
+    def __init__(self, nombre1, nombre2):
+        self.nombre1 = nombre1
+        self.nombre2 = nombre2
+
+exemple = operation (10,5)
+print(f'Le nombre1 est : {exemple.nombre1}')
+print(f'Le nombre2 est : {exemple.nombre2}')
