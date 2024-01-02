2aec41fac0bc4d9eee133d28b79885d6f234c774
jean-renoul <jean.renoul@laplateforme.io>
2024-01-02 12:10:39 +0100

    job07 et 08 finis

---
+++ jour01/job08.py
@@ -0,0 +1,29 @@
+class Cercle:
+    def __init__(self, rayon):
+        self.rayon = rayon
+    def changerRayon(self, rayon):
+        self.rayon = rayon
+    def circonference(self):
+        return 2 * 3.14 * self.rayon
+    def aire(self):
+        return 3.14 * self.rayon**2
+    def diametre(self):
+        return 2 * self.rayon
+    def afficherInfos(self):
+        print(f"Rayon : {self.rayon}")
+        print(f"Circonférence : {self.circonference()}")
+        print(f"Aire : {self.aire()}")
+        print(f"Diamètre : {self.diametre()}")
+
+cercle1 = Cercle(4)
+cercle2 = Cercle(7)
+
+cercle1.afficherInfos()
+print (f"La circonférence du cercle1 est {cercle1.circonference()}")
+print (f"L'aire du cercle1 est {cercle1.aire()}")
+print (f"Le diamètre du cercle1 est {cercle1.diametre()}")
+
+cercle2.afficherInfos()
+print (f"La circonférence du cercle2 est {cercle2.circonference()}")
+print (f"L'aire du cercle2 est {cercle2.aire()}")
+print (f"Le diamètre du cercle2 est {cercle2.diametre()}")
