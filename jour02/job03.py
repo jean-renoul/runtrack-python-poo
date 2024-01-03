1f015e14c91caefcd1e129929dceb76e0b49c0b2
jean-renoul <jean.renoul@laplateforme.io>
2024-01-03 10:20:42 +0100

    job03 fini

---
+++ jour02/job03.py
@@ -0,0 +1,52 @@
+class livre:
+    def __init__(self,titre, auteur, nombre_de_pages,disponible = True):
+        self.__titre = titre
+        self.__auteur = auteur
+        self.__nombre_de_pages = nombre_de_pages
+        self.__disponible = disponible
+
+    def verification(self):
+        return self.__disponible
+        
+    def emprunter(self):
+        if self.verification() == True:
+            self.__disponible = False
+            print ("Le livre a été emprunté")
+            return self.__disponible
+        else:
+            print ("Le livre est indisponible")
+    
+    def rendre(self):
+        if not self.verification():
+            self.__disponible = True
+            print ("Le livre a été rendu")
+            return self.__disponible
+        else:
+            print ("Le livre est déjà disponible")
+    
+    def get_titre(self):
+        return self.__titre
+    
+    def get_auteur(self):
+        return self.__auteur
+    
+    def get_nombre_de_pages(self):
+        return self.__nombre_de_pages
+    
+    def set_titre(self, titre):
+        self.__titre = titre
+
+    def set_auteur(self, auteur):
+        self.__auteur = auteur
+
+    def set_nombre_de_pages(self, nombre_de_pages):
+        if nombre_de_pages > 0 and nombre_de_pages == int:
+            self.__nombre_de_pages = nombre_de_pages
+        else:
+            print("Nombre de pages incorrect")
+
+exemple = livre("Le seigneur des anneaux", "J.R.R. Tolkien", 1000)
+
+print (exemple.verification())
+print (exemple.emprunter())
+print (exemple.rendre())
